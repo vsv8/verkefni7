6f603fca917327dfaa44f8461a2c4c7334a14a88
vsv8 <42941645+vsv8@users.noreply.github.com>
2018-10-30 19:32:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+/**
+ * Verkefni 7 – Reikniæfingarforrit
+ *
+ * Forrit sem æfir hraða í að reikna einföld dæmi
+ */
+
+// fasti sem segir til um hve marga leiki eigi að spila
+const GAMES_TO_PLAY = 10;
+
+/**
+ * Birtir upplýsingar um leik og eftir að notandi samþykkir spilar fyrsta leik
+ * með kalli í play().
+ * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
+ */
+function start() {
+  alert('Markmiðið er að svara eins mörgum af 10 dæmum rétt eins hratt og mögulegt er. Vinsamlegast ýttu á "OK" þegar þú ert tilbúinn');
+  do {
+  	play();
+  } while(confirm('Spila annan?'));
+}
+
+/**
+ * Spilar einn leik. Heldur utan um hvenær leikur byrjaði, hvenær endar og
+ * fjölda réttra svara. Eftir leik eru birtar upplýsingar um niðurstöðu:
+ *   Þú svaraðir X af 10 dæmum rétt á Y sekúndum
+ *   Meðalrétt svör á sekúndu eru Z
+ * Þar sem Y og Z hafa tvo aukastafi.
+ *
+ * Ef notandi ýtir á "Cancel" í leik eru skilaboðin "Hætt í leik." birt og engar
+ * upplsýingar um niðurstöður.
+ *
+ */
+function play() {
+	const timi1 = performance.now();
+	const spurningar = [ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask(), ask()];
+  heild = spurningar.length;
+  rett = spurningar.filter(Boolean).length;
+  const timi2 = performance.now();
+  const lokatimi = (timi2 - timi1) / 1000;
+	alert("Þú svaraðir " + rett + " af " + heild + " rétt á " + Math.round(lokatimi * 100) / 100 + " sekúndum");
+}
+
+/**
+ * Spyr einnar spurningar og skilar upplýsingum um svar (mögulega með því að
+ * nota true, false og null ef notandi hættir). Birtir notanda propmpt til að
+ * svara í og túlkar svarið yfir í tölu.
+ *
+ * Mögulegar spurningar eru:
+ * - `+` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
+ * - `-` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
+ * - `*` dæmi þar sem báðar tölur geta verið á bilinu `[1, 10]`
+ * - `/` dæmi þar sem fyrri tala er á bilinu `[2, 10]` og seinni talan er fyrri
+ *   talan sinnum tala á bilinu `[2, 10]` þ.a. svarið verði alltaf heiltala
+ *
+ * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
+ */
+
+function ask() {
+  const fyrri = randomNumber(1, 100);
+  const seinni = randomNumber(1, 100);
+  const virki = ["*", "+", "/", "-"][Math.floor(Math.random()*4)];
+  return prompt(fyrri + " " + virki + " " + seinni + "?") == eval(fyrri + virki + seinni);
+}
+
+/**
+ * Skilar tölu af handahófi á bilinu [min, max]
+ */
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Byrjar leik
+start();
